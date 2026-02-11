1a21b40f1f818beda821b3d9ea1bdbd9e96f1ec4
vitor pinto <vitor.pinto@ecoles-epsi.net>
2026-02-11 18:29:49 +0100

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['https://tp-cloud-2-426914906044.europe-west9.run.app','https://tp-cloud-final-426914906044.europe-west9.run.app'],
+    credentials: true,
+  });
   const port = process.env.PORT || 3000;
   await app.listen(port);
 }
